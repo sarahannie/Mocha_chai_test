fc42f21bbd718037a1c4771a70394b9d768fa0f6
sarahannie <sarahndianekwu@gmail.com>
2023-06-27 08:46:16 +0100

    factorial  testing

--- multiply.js
+++ multiply.js
@@ -1,5 +1,14 @@
-function multiply(a, b) {
-    return a*b
+// function multiply(a, b) {
+//     return a*b
+// }
+
+function factorial (a){
+    let number =1;
+    for(let i = 1; i<= a; i++){
+        number *= i;
+        console.log(number)
+    }
+    return number;
 }
 
-module.exports = multiply;
+module.exports = factorial;
